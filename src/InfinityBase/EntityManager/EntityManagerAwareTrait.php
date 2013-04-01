fc3ddd69e210d15fa606f743785c1661509c685e
ise <recklessthief@gmail.com>
2013-04-01 15:57:58 +0100

    Addition of base files

---
+++ src/InfinityBase/EntityManager/EntityManagerAwareTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace InfinityBase\EntityManager;
+
+use Doctrine\ORM\EntityManager;
+use InfinityBase\ServiceManager\ServiceLocatorAwareTrait;
+
+trait EntityManagerAwareTrait
+{
+    use ServiceLocatorAwareTrait;
+
+    /**
+     * @var EntityManager
+     */
+    private $_entityManager;
+
+    /**
+     * Get entity manager
+     *
+     * @return EntityManager
+     */
+    public function getEntityManager()
+    {
+        if (!isset($this->_entityManager)) {
+            $this->_entityManager = $this->getServiceLocator()
+                    ->get('Doctrine\ORM\EntityManager');
+        }
+        return $this->_entityManager;
+    }
+}
---
+++ src/InfinityBase/ServiceManager/ServiceLocatorAwareTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace InfinityBase\ServiceManager;
+
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+trait ServiceLocatorTrait
+{
+    /**
+     * @var ServiceLocatorInterface
+     */
+    private $_serviceLocator;
+
+
+    /**
+     * Retrieve the service locator
+     *
+     * @return ServiceLocatorInterface
+     */
+    public function getServiceLocator()
+    {
+        return $this->_serviceLocator;
+    }
+
+    /**
+     * Set the service locator
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return AbstractForm
+     */
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+    {
+        $this->_serviceLocator = $serviceLocator;
+        return $this;
+    }
+
+}
